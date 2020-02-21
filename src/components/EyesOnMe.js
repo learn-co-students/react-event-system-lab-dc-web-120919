bc516b19c8aadc4438f2ac077ca8b882387eb0cf
github username <github email address>
2020-02-21 12:52:53 -0500

    keypad works

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+    render() {
+        return(
+            <div>Hello</div>
+        )
+    }
+}
+
+export default EyesOnMe
